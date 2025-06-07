44d1bcca09a7d079e903f9df85442fe80e777962
Aestu <aryapangestu2024@gmail.com>
2025-06-07 23:39:04 +0700

    Create 3LmengubahList.py

---
+++ strukturDataAlgoritma/ArraysAndLinkedList/Arrays/3LmengubahList.py
@@ -0,0 +1,10 @@
+warna = ["Merah", "Hijau", "Biru", "Kuning"]
+print(f"List warna awal: {warna}")
+
+# Mengubah elemen pada indeks 1 dari "Hijau" menjadi "Ungu"
+warna[1] = "Ungu"
+print(f"List warna setelah diubah: {warna}") # Output: ['Merah, 'Ungu', 'Biru', 'Kuning']
+
+# Mengubah elemen terakhir
+warna[-1] = "Oranye"
+print(f"List warna setelah mengubah terakhir: {warna}") # Output: ['Merah', 'Ungu', 'Biru', 'Oranye']
